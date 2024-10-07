3aeaf116fd932c0f4e9b682a95ec490c05982e35
thaisgebel <maria.thais.gebel@gmail.com>
2024-10-06 21:25:00 -0300

    feat(homePage):finish function filter by name,add function of prev and next buttons

--- homePage.js
+++ homePage.js
@@ -1,48 +1,51 @@
 document.addEventListener("DOMContentLoaded", function () {
-  fetch("https://pokeapi.co/api/v2/pokemon?limit=25&offset=0")
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=10277&offset=0")
     .then((response) => response.json())
     .then((data) => {
-      createCardPokemon(data.results);
-      getInformationPokemon(
-        "https://pokeapi.co/api/v2/pokemon?limit=10277&offset=0"
-      ).then((infoGlobal) => {
-        informationGlobal = infoGlobal;
-      });
+      informationGlobal = data;
+      filterBarByName(data, "");
+      createCardPokemon(pokemonsFiltradospaginacion);
     });
 });
 
 let informationGlobal = [];
-
 const containerCardsPokemons = document.getElementById(
   "container-cards-pokemons"
 );
-
 function getInformationPokemon(url) {
   return fetch(url).then((response) => response.json());
 }
-
-//
 
 const searchBarByName = document.getElementById("search-bar-by-name");
 const buttonBarByName = document.getElementById("button-bar-by-name");
 
 buttonBarByName.addEventListener("click", function () {
-  let pokemonsFilter = filterBarByName(
+  let pokemonsFilterByName = filterBarByName(
     informationGlobal,
     searchBarByName.value
   );
-  createCardPokemon(pokemonsFilter);
+  createCardPokemon(pokemonsFilterByName);
 });
 
+let pokemonsFiltradospaginacion = [];
+
 function filterBarByName(information, filter) {
+  currentPagination = 0;
+  pokemonsFiltradospaginacion = [];
   let namesfiltered = information.results.filter(function (value) {
     return value.name.includes(filter);
   });
-  return namesfiltered;
-}
 
-//
-//
+  let numPage = Math.ceil(namesfiltered.length / 25);
+
+  for (let index = 0; index < numPage; index++) {
+    pokemonsFiltradospaginacion.push([]);
+    for (let p = 25 * index; p < 25 * index + 25; p++) {
+      pokemonsFiltradospaginacion[index].push(namesfiltered[p]);
+    }
+  }
+  return pokemonsFiltradospaginacion;
+}
 
 function getTypes(data) {
   let arrayTypes = data.types.map(function (value) {
@@ -55,7 +58,6 @@ function createTypes(individualPokemon, cardPokemon) {
   let arrayTypes = getTypes(individualPokemon);
   let containerTypes = document.createElement("div");
   cardPokemon.append(containerTypes);
-
   for (let index = 0; index < arrayTypes.length; index++) {
     let typePokemon = document.createElement("p");
     typePokemon.classList.add(arrayTypes[index]);
@@ -65,34 +67,50 @@ function createTypes(individualPokemon, cardPokemon) {
   }
 }
 
-//
+let currentPagination = 0;
 
 function createCardPokemon(information) {
   containerCardsPokemons.innerHTML = "";
-  for (let index = 0; index < information.length; index++) {
+  for (let index = 0; index < 25; index++) {
+    if (information[currentPagination][index] === undefined) {
+      break;
+    }
     let cardPokemon = document.createElement("div");
     cardPokemon.classList.add("card-pokemon");
-
     let containerNamesAndIds = document.createElement("div");
     let namePokemon = document.createElement("p");
     let idPokemon = document.createElement("span");
-
     let imagePokemon = document.createElement("img");
-
     containerCardsPokemons.append(cardPokemon);
-
     cardPokemon.append(containerNamesAndIds);
     containerNamesAndIds.append(namePokemon);
     containerNamesAndIds.append(idPokemon);
-
     cardPokemon.append(imagePokemon);
-
-    getInformationPokemon(information[index].url).then((individualPokemon) => {
-      namePokemon.textContent = individualPokemon.name;
-      idPokemon.textContent = `ID: ${individualPokemon.id}`;
-      createTypes(individualPokemon, cardPokemon);
-      imagePokemon.src =
-        individualPokemon.sprites.other["official-artwork"].front_default;
-    });
+    getInformationPokemon(information[currentPagination][index].url).then(
+      (individualPokemon) => {
+        namePokemon.textContent = individualPokemon.name;
+        idPokemon.textContent = `ID: ${individualPokemon.id}`;
+        createTypes(individualPokemon, cardPokemon);
+        imagePokemon.src =
+          individualPokemon.sprites.other["official-artwork"].front_default;
+      }
+    );
   }
 }
+const buttonPreviousPage = document.getElementById("button-previous-page");
+
+buttonPreviousPage.addEventListener("click", function () {
+  if (currentPagination > 0) {
+    currentPagination -= 1;
+    createCardPokemon(pokemonsFiltradospaginacion);
+  }
+});
+
+const buttonNextPage = document.getElementById("button-next-page");
+
+buttonNextPage.addEventListener("click", function () {
+  if (currentPagination < pokemonsFiltradospaginacion.length - 1) {
+    currentPagination += 1;
+    createCardPokemon(pokemonsFiltradospaginacion);
+  }
+});
